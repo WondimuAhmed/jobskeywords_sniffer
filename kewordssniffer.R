12e6398088bda6549b46b0bddc1f6b610d0a8fb4
WondimuAhmed <44304760+WondimuAhmed@users.noreply.github.com>
2022-01-30 10:35:38 -0500

    check if working

--- kewordssniffer.R
+++ kewordssniffer.R
@@ -1,3 +1,5 @@
 # Goal of the project is to develop a shiny application that can help user extract keywords in a job description or a resume. 
 ## Aquire important packages 
 # get files
+
+# Let's identify some strategies 
